658bdfeec14a1d5fc3abee4bb46644209e2d0787
kangdiancp <kangdiancp@gmail.com>
2021-05-24 10:45:46 +0700

    init repo

---
+++ server/express.js
@@ -0,0 +1,55 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+
+
+const app = express()
+
+// parse body params and attache them to req.body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+// use helmet spy bisa dikenali SEO
+app.use(helmet())
+// secure apps by setting various HTTP headers
+app.use(compress())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+
+
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+
+app.use("/minpro/", (req, res) => {
+    res.send("Hello HR-Fullstack JS")
+});
+
+// #middleware
+app.use(async (req, res, next) => {
+    req.context = {models};
+    next();
+});
+
+
+
+
+
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ "error": err.name + ": " + err.message })
+    } else if (err) {
+        res.status(400).json({ "error": err.name + ": " + err.message })
+        console.log(err)
+    }
+})
+
+export default app
